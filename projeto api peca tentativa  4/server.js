2c709751b2d07a6341fd3ca4f4ff62bef501b42c
Sidney C de Oliveira Jr <sidneycardosojo@gmail.com>
2024-05-27 23:48:04 -0300

    Versão 4

--- projeto api peca tentativa  4/server.js
+++ projeto api peca tentativa  4/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const pecaRoutes = require('../projeto api peca tentativa  4/APICRUD/Routes/pecasRoutes');  // Verifique este caminho
+const pecaRoutes = require('../projeto api peca tentativa  4/APICRUD/Routes/pecasRoutes');  
 
 app.use(express.json());
 app.use('/pecas', pecaRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`O servidor ira rodar na porta ${PORT}`);
 });
